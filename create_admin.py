edc10b8ad7799bc480c57ddd0a1b911dd900f77a
cabralipe <cabralipe@outlook.com>
2025-01-27 21:24:16 -0300

    Adicionando .gitignore e arquivos iniciais do projeto

---
+++ create_admin.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
+from app import app, db  # Certifique-se de que o `app` está configurado corretamente
+from models import Usuario
+
+# Certifique-se de que o contexto do aplicativo está ativo
+with app.app_context():
+    # Verifica se o administrador já existe
+    admin_existente = Usuario.query.filter_by(email="admin@exemplo.com").first()
+    
+    if admin_existente:
+        print("Administrador já existe no banco de dados.")
+    else:
+        # Criando um Administrador Padrão
+        admin = Usuario(
+            nome="Administrador",
+            cpf="000.000.000-00",  # Certifique-se de colocar um CPF válido
+            email="admin@exemplo.com",
+            senha=generate_password_hash("123456", method='pbkdf2:sha256'),
+            formacao="Gestão",
+            tipo="admin"
+        )
+
+        # Adicionando ao banco de dados
+        db.session.add(admin)
+        db.session.commit()
+
+        print("Administrador criado com sucesso!")
